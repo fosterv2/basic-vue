028bd2ff62b6430c9c908335ae670aa352311f6d
fosterv2 <fosterv2@outlook.com>
2020-10-02 13:45:41 -0700

    Vue tutorial: Introduction

--- js/index.js
+++ js/index.js
@@ -1,13 +1,1 @@
-var  vm = new Vue({
-    el: '#vue_det',
-    data: {
-       firstname : "Ria",
-       lastname  : "Singh",
-       address    : "Mumbai"
-    },
-    methods: {
-       mydetails : function() {
-          return "I am "+this.firstname +" "+ this.lastname;
-       }
-    }
- })
+
