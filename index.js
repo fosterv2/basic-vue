4b0e19b68d4fb9b15b370c1b7175d4c90c66f562
fosterv2 <fosterv2@outlook.com>
2020-10-20 14:29:07 -0700

    Vue.js Instances

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+new Vue({
+  data: {
+    a: 1
+  },
+  created: function () {
+    // `this` points to the vm instance
+    console.log('a is: ' + this.a)
+  }
+})
+
+var obj = {
+  foo: 'bar'
+}
+
+Object.freeze(obj)
+
+var bar = new Vue({
+  el: '#bar',
+  data: obj
+})
+
 var app = new Vue({ 
   el: '#app',
   data: {
