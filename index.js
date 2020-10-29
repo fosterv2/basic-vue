4399cfe0d6f42b100a2e384e12914a8c70d0772b
fosterv2 <fosterv2@outlook.com>
2020-10-29 16:55:02 -0700

    Vue.js Event Handling: Modifiers

--- index.js
+++ index.js
@@ -1,46 +1,35 @@
-var example1 = new Vue({
-  el: '#example-1',
+var modifiers = new Vue({
+  el: '#modifiers',
   data: {
     counter: 0
-  }
-})
-
-var example2 = new Vue({
-  el: '#example-2',
-  data: {
-    name: 'Vue.js'
   },
-  // define methods under the `methods` object
   methods: {
-    greet: function (event) {
-      // `this` inside methods points to the Vue instance
-      alert('Hello ' + this.name + '!')
-      // `event` is the native DOM event
-      if (event) {
-        alert(event.target.tagName)
-      }
-    }
-  }
-})
-
-var example3 = new Vue({
-  el: '#example-3',
-  methods: {
-    say: function (message) {
+    warn: function (message) {
       alert(message)
     }
   }
 })
 
-var example4 = new Vue({
-  el: '#example-4',
+var keys = new Vue({
+  el: '#keys',
+  data: {
+    counter: 0
+  },
   methods: {
-    warn: function (message, event) {
-      // now we have access to the native event
-      if (event) {
-        event.preventDefault()
-      }
-      alert(message)
+    add: function () {
+      this.counter++
+    },
+    subtract: function () {
+      this.counter--
+    }
+  }
+})
+
+var exact = new Vue({
+  el: '#exact',
+  methods: {
+    onClick: function () {
+      console.log('Clicked!')
     }
   }
 })
