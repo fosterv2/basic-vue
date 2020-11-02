bacc6891f041f4d61674c855c80216daf497b58d
fosterv2 <fosterv2@outlook.com>
2020-11-02 12:50:11 -0800

    Vue.js Components Basics

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
-var checkbox = new Vue({
-  el: "#checkbox",
+// Define a new component called button-counter
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+Vue.component('blog-post', {
+  props: ['title'],
+  template: '<h3>{{ title }}</h3>'
+})
+
+Vue.component('blog-post-two', {
+  props: ['post'],
+  template: `
+    <div class="blog-post">
+      <h3>{{ post.title }}</h3>
+      <button v-on:click="$emit('enlarge-text')">
+        Enlarge text
+      </button>
+      <div v-html="post.content"></div>
+    </div>
+  `
+})
+
+Vue.component('custom-input', {
+  props: ['value'],
+  template: `
+    <input
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    >
+  `
+})
+
+new Vue({ el: '#components-demo' })
+
+// new Vue({ el: '#titles' })
+
+new Vue({
+  el: '#blog-post-demo',
   data: {
-    toggle: "no"
+    posts: [
+      { id: 1, title: 'My journey with Vue' },
+      { id: 2, title: 'Blogging with Vue' },
+      { id: 3, title: 'Why Vue is so fun' }
+    ]
   }
 })
 
-var radio = new Vue({
-  el: "#radio",
+new Vue({
+  el: '#blog-posts-events-demo',
   data: {
-    picked: "",
-    pick: "",
-    a: "I am A",
-    b: "I am B"
+    posts: [
+      { id: 1, title: 'My journey with Vue', content: "...content..." },
+      { id: 2, title: 'Blogging with Vue', content: "...content..." },
+      { id: 3, title: 'Why Vue is so fun', content: "...content..." }
+    ],
+    postFontSize: 1
   }
 })
 
-var select = new Vue({
-  el: "#select",
+new Vue({
+  el: "#events",
   data: {
-    selected: ""
-  }
-})
-
-var modifiers = new Vue({
-  el: "#modifiers",
-  data: {
-    age: 0,
-    msg: ""
+    searchText: "searching..."
   }
 })
---
+++ js/value.js
@@ -0,0 +1,31 @@
+var checkbox = new Vue({
+  el: "#checkbox",
+  data: {
+    toggle: "no"
+  }
+})
+
+var radio = new Vue({
+  el: "#radio",
+  data: {
+    picked: "",
+    pick: "",
+    a: "I am A",
+    b: "I am B"
+  }
+})
+
+var select = new Vue({
+  el: "#select",
+  data: {
+    selected: ""
+  }
+})
+
+var modifiers = new Vue({
+  el: "#modifiers",
+  data: {
+    age: 0,
+    msg: ""
+  }
+})
